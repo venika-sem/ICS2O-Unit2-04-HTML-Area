4b5e25a2938081b1dbe9f3c189a15c6c1404710d
venika-sem <99354917+venika-sem@users.noreply.github.com>
2022-03-25 16:41:15 +0000

    changed script.js

--- js/script.js
+++ js/script.js
@@ -8,15 +8,14 @@
 /**
  * This function calculates area of triangle.
  */
-function calculateArea() {
+function calculate() {
   // input
   const base = parseInt(document.getElementById('base-of-triangle').value)
   const height = parseInt(document.getElementById('height-of-triangle').value)
 
   // process
-  const area = 2 / (base * height)
+  const area = (base * height) / 2
 
   // output
-  document.getElementById('area').innerHTML =
-    "<p>Your area is: '2 / (' + base + '+' + height + ') = ' + area + ' or ' + area + ' cm².'</p>"
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
 }
